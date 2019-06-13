766b12c5f42abd8d4bf15e2c9fc98c2982ced712
kuaifan <aipaw@live.cn>
2019-06-13 14:27:24 +0800

    no msg

--- .vuepress/components/Markets.vue
+++ .vuepress/components/Markets.vue
@@ -1,15 +1,39 @@
 <template>
     <div class="eeui-markets">
-        <iframe ref="myFrame" src="//console.eeui.app/#/markets" frameborder="0" scrolling="no" class="eeui-frame"></iframe>
+        <div ref="myLoading" class="eeui-loading" :class="'eeui-loading-' + loading"></div>
+        <iframe ref="myFrame" class="eeui-frame" src="//console.eeui.app/#/markets" frameborder="0" scrolling="no" @load="loadFinish" :style="{height:height + 'px'}"></iframe>
     </div>
 </template>
 
 <style lang="stylus">
     .eeui-markets {
-        width 100%;
+        width 100%
+        position relative
         .eeui-frame {
-            width 100%;
-            height 580px
+            width 100%
+            border 0
+        }
+        .eeui-loading {
+            position absolute
+            top 0
+            left 0
+            width 0
+            height 2px
+            opacity 1
+            background-color #3fcc25
+            transition all 5s ease-in-out
+            &.eeui-loading-start {
+                width 90%
+            }
+            &.eeui-loading-end {
+                width 100%
+                transition all .3s ease-in-out
+            }
+            &.eeui-loading-finish {
+                opacity 0
+                width 100%
+                transition all .1s ease-in-out
+            }
         }
     }
 </style>
@@ -17,6 +41,8 @@
     export default {
         data() {
             return {
+                height: 580,
+                loading: ''
             }
         },
         mounted() {
@@ -24,9 +50,17 @@
             parent.style.setProperty('max-width', 'none', 'important');
             parent.style.setProperty('padding', '0', 'important');
             parent.nextElementSibling.style.setProperty('display', 'none', 'important');
-            window.addEventListener('message', (e) => {
-                this.$refs.myFrame && (this.$refs.myFrame.style.height = e.data.height + 'px');
-            }, false);
+            window.addEventListener('message', (e) => { this.height = e.data.height; }, false);
+            setTimeout(() => { this.loading = 'start'; }, 100);
+        },
+        methods: {
+            loadFinish() {
+                this.$refs.myLoading.addEventListener("transitionend", (e) => {
+                    setTimeout(() => { this.loading = 'finish'; }, 100);
+                }, false);
+                setTimeout(() => { this.loading = 'finish'; }, 1000);
+                this.loading = 'end';
+            }
         }
     }
 </script>
